ca66d00f0df278367d7e0c6ecb2de9022cac4c45
alro9829 <alro9829@colorado.edu>
2022-02-06 13:55:19 -0700

    staff, clerk, store class definitions

---
+++ OOAD/project2-2/src/Staff/Staff.java
@@ -0,0 +1,15 @@
+package Staff;
+
+public class Staff {
+
+    private String name;
+
+    public Staff(String name1) {
+        name = name1;
+    }
+
+    public String get_name(){
+        return name;
+    }
+
+}
