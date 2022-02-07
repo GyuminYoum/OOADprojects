17868c664874ba335239df4dacb0ba5a30b02675
alro9829 <alro9829@colorado.edu>
2022-02-06 22:21:31 -0700

    add and removeStaff methods

--- OOAD/project2-2/src/Store.java
+++ OOAD/project2-2/src/Store.java
@@ -1,19 +1,31 @@
 import Items.Items;
+import Staff.Staff;
 import Staff.Clerk;
+
+import java.util.ArrayList;
 
 public class Store {
     private int Cash_Register;
-    private Items[] Inventory;
+    private ArrayList<Items> Inventory;
     private float Inventory_value;
-    private Clerk staff_member;
+    private ArrayList<Staff> staff_member;
     private int daysPassed;
-    private Items[] Items_sold;
+    private ArrayList<Items> Items_sold;
     private float total_salePrice;
     private float money_added;
 
     public void ManageStaff() {
+
     }
 
     public void Report(){
     }
+
+    public void AddStaff(Staff worker) {
+        staff_member.add(worker);
+    }
+    public void RemoveStaff(Staff worker) {
+        staff_member.remove((staff_member.indexOf(worker)));
+    }
+
 }
