67e81897226e3001d254fedc3d721e86f64e05a8
GyuminYoum <GyuminYoum@users.noreply.github.com>
2022-02-08 18:44:10 -0700

    changed all the Double to double, Integer to int, Boolean to boolean

--- OOAD/project2-2/src/Instruments_Items/Stringed.java
+++ OOAD/project2-2/src/Instruments_Items/Stringed.java
@@ -3,16 +3,16 @@ package Instruments_Items;
 import Items.Instruments;
 
 public class Stringed extends Instruments {
-    private Boolean electric;
+    private boolean electric;
 
-    public Stringed(String name1, Double price1,  Boolean nou, Integer day1, Integer con1, Boolean bool1) {
+    public Stringed(String name1, double price1,  boolean nou, int day1, int con1, boolean bool1) {
         super(name1, price1, nou, day1, con1);
         electric=bool1;
     }
-    public Boolean get_electric(){
+    public boolean get_electric(){
         return electric;
     }
-    public void set_electric(Boolean bool1){
+    public void set_electric(boolean bool1){
         electric=bool1;
     }
 }
