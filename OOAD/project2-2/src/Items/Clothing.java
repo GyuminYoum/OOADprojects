91acc7fdee6a01d16a323a881aadfdfb0cd944f1
GyuminYoum <GyuminYoum@users.noreply.github.com>
2022-02-14 22:40:39 -0700

    directory change

--- OOAD/project2-2/src/Items/Clothing.java
+++
@@ -1,9 +0,0 @@
-package Items;
-
-public class Clothing extends Items {
-
-    public Clothing(String name1, double price1,  boolean nou, int day1, int con1) {
-        super(name1, price1, nou, day1, con1);
-    }
-}
-
