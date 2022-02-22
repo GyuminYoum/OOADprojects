053cbc56cd90e64375253da4b2fd43a67a113db8
alro9829 <alro9829@colorado.edu>
2022-02-22 15:03:16 -0700

    working on decorator

---
+++ project3/OOAD/project2-2/src/Instruments_Items/Stringed_Items/StringDecorator.java
@@ -0,0 +1,11 @@
+package Instruments_Items.Stringed_Items;
+
+import Instruments_Items.Stringed;
+
+public class StringDecorator extends Stringed {
+
+    public StringDecorator(String name1, double price1,  boolean nou, int day1, int con1, boolean bool1) {
+        super(name1, price1, nou, day1, con1, bool1);
+    }
+
+}
