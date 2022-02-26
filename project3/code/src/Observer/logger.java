03e063693f2a78686d2b8e306e425ae58acd89a7
alro9829 <alro9829@colorado.edu>
2022-02-25 18:09:52 -0700

    trying to pull from main again

--- project3/code/src/Observer/logger.java
+++ project3/code/src/Observer/logger.java
@@ -16,37 +16,38 @@ public class logger implements Observer, Writer, Display {
     private String content;
     private String file_name;
 
-    public void makeTxt(String str1){
-        File file=new File(str1);
-        file_name=str1;
-        try{
+    public void makeTxt(String str1) {
+        File file = new File(str1);
+        file_name = str1;
+        try {
             Files.deleteIfExists(file.toPath());
             file.createNewFile();
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             System.err.println(e);
         }
 
     }
-    public void update(String content1){
-        this.content=content1;
+
+    public void update(String content1) {
+        this.content = content1;
         display();
         Write();
     }
+
     //source: https://stackoverflow.com/questions/9961292/write-to-text-file-without-overwriting-in-java
     //writes to text file
-    public void Write(){
+    public void Write() {
         //write it
         //FileUtils.
-        File file=new File(file_name);
+        File file = new File(file_name);
 
-        try{
-            FileWriter scribe=new FileWriter(file,true);
-            BufferedWriter bufferedScribe= new BufferedWriter(scribe);
+        try {
+            FileWriter scribe = new FileWriter(file, true);
+            BufferedWriter bufferedScribe = new BufferedWriter(scribe);
             bufferedScribe.write(content);
             bufferedScribe.newLine();
             bufferedScribe.flush();
-        }catch(Exception e){
+        } catch (Exception e) {
             System.err.println(e);
         }
 
@@ -78,9 +79,11 @@ public class logger implements Observer, Writer, Display {
 
 
     }
-    public void display(){
+
+    public void display() {
         System.out.println(content);
     }
+}
 
 
 
