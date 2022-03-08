06e400780313a0d1dc28064ecab323944667729b
alro9829 <70238044+alro9829@users.noreply.github.com>
2022-03-07 17:01:03 -0700

    Tracker fixed

--- project4/code/src/Observer/Tracker.java
+++ project4/code/src/Observer/Tracker.java
@@ -2,6 +2,7 @@ package Observer;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Objects;
 
 import Staff.Clerk;
 import Staff.Electronic;
@@ -9,117 +10,78 @@ import Staff.Haphazard;
 import Staff.Manual;
 import Store.Store;
 //Tracker implements the Observer to keep track of how many items each clerk has sold, purchased, and damaged
+//Eager loading singleton
 public class Tracker implements Observer{
-
-    public HashMap<String, ArrayList<Integer>> north = new HashMap<String, ArrayList<Integer>>();
-    public HashMap<String, ArrayList<Integer>> south = new HashMap<String, ArrayList<Integer>>();
-    //private Store location;
-
-
-    ArrayList<String> members = new ArrayList<String>();
-
-    /*public Tracker(){
-        initialize(members);
-    }*/
 
     private static Tracker instance = new Tracker();
 
     private Tracker() {
-    initialize(members);
-    System.out.println("Tracker instantiated");
+        //System.out.println("Tracker instantiated");
     }
 
     public static Tracker GetInstance() {
         return instance;
     }
 
-    //initialize HashMap at the beginning of simulation
-    public void initialize(ArrayList<String> members) {
-        //Store Store=location;
-        //for every clerk
-        members.add("Velma");
-        members.add("Shaggy");
-        members.add("Daphne");
-        members.add("Justin");
-        members.add("Freddy");
-        for (String c: members) {
-            ArrayList<Integer> intList = new ArrayList<Integer>();
-            for (int i = 0; i < 3; i++) {
-                intList.add(0);
-            }
-            //Key is clerk name, {0,0,0} represents items sold/purchased/damaged
-            north.put(c, intList);
-            south.put(c, intList);
-        }
-    }
-
     public void update(String s, Store Store) {
         //Store Store=location;
-        int curr;
-        if (Store.get_location() == "North") {
-            switch(s) {
-                case "sold":
+        Clerk clerk = Store.get_OnShift();
+        //check which store is calling update()
+        if (Objects.equals(Store.get_location(), "North")) {
+            switch (s) {
+                case "sold" -> {
                     //increment sold value for Store.get_onShift().get_name()
-                    curr = north.get(Store.get_OnShift().get_name()).get(0) + 1;
-                    north.get(Store.get_OnShift().get_name()).set(0, curr);
-
-                    break;
-                case "purchased":
-                    curr = north.get(Store.get_OnShift().get_name()).get(1) + 1;
-                    north.get(Store.get_OnShift().get_name()).set(1, curr);
-                    //increment purchased value for Store.get_onShift().get_name()
-                    break;
-                case "damaged":
-                    curr = north.get(Store.get_OnShift().get_name()).get(2) + 1;
-                    north.get(Store.get_OnShift().get_name()).set(2, curr);
-                    //increment damaged value for Store.get_onShift().get_name()
-                    break;
+                    clerk.set_SN(clerk.get_SN() + 1);
+                }
+                case "purchased" -> {
+                    clerk.set_BN(clerk.get_BN() + 1);
+                }
+                //increment purchased value for Store.get_onShift().get_name()
+                case "damaged" -> {
+                    clerk.set_DN(clerk.get_DN() + 1);
+                }
+                //increment damaged value for Store.get_onShift().get_name()
             }
+
         } else {
             switch (s) {
                 case "sold" -> {
                     //increment sold value for Store.get_onShift().get_name()
-                    curr = south.get(Store.get_OnShift().get_name()).get(0) + 1;
-                    south.get(Store.get_OnShift().get_name()).set(0, curr);
+                    clerk.set_SS(clerk.get_SS() + 1);
                 }
                 case "purchased" -> {
-                    curr = south.get(Store.get_OnShift().get_name()).get(1) + 1;
-                    south.get(Store.get_OnShift().get_name()).set(1, curr);
+                    clerk.set_BS(clerk.get_BS() + 1);
                 }
                 //increment purchased value for Store.get_onShift().get_name()
                 case "damaged" -> {
-                    curr = south.get(Store.get_OnShift().get_name()).get(2) + 1;
-                    south.get(Store.get_OnShift().get_name()).set(2, curr);
+                    clerk.set_DS(clerk.get_DS() + 1);
                 }
                 //increment damaged value for Store.get_onShift().get_name()
             }
         }
-
     }
 
     public void display(Store Store) {
-        if (Store.get_location() == "North") {
-            int days1=Store.get_daysPassed()-1;
+        int days1=Store.get_daysPassed()-1;
+
+        //check which Store is calling display()
+        if (Objects.equals(Store.get_location(), "North")) {
             System.out.println("North Store");
             System.out.println("Day: " + days1);
             System.out.println("Clerk | Items Sold | Items Purchased | Items Damaged");
             for (Clerk c : Store.get_ClerkMember()) {
-                //print out each value in the ArrayList mapped to that clerk
-                ArrayList<Integer> output = north.get(c.get_name());
-                System.out.printf(c.get_name() + " | " + output.get(0) + "           | " + output.get(1) + "                | " + output.get(2) + "\n");
+                //print out the Clerk's soldNorth, boughtNorth, dmgNorth values
+                System.out.printf(c.get_name() + " | " + c.get_SN() + "           | " + c.get_BN() + "                | " + c.get_DN() + "\n");
             }
         } else {
-            int days1=Store.get_daysPassed()-1;
             System.out.println("South Store");
             System.out.println("Day: " + days1);
             System.out.println("Clerk | Items Sold | Items Purchased | Items Damaged");
             for (Clerk c : Store.get_ClerkMember()) {
-                //print out each value in the ArrayList mapped to that clerk
-                ArrayList<Integer> output = south.get(c.get_name());
-                System.out.printf(c.get_name() + " | " + output.get(0) + "           | " + output.get(1) + "                | " + output.get(2) + "\n");
+                //print out the Clerk's soldSouth, boughtSouth, dmgSouth values
+                System.out.printf(c.get_name() + " | " + c.get_SS() + "           | " + c.get_BS() + "                | " + c.get_DS() + "\n");
             }
         }
-        //Store Store=location;
         //print out table headers
 
     }
