0c1f9542c641b1b4304c003dada736dfc6d17d08
alro9829 <70238044+alro9829@users.noreply.github.com>
2022-03-09 00:02:05 -0700

    output.txt added, comments

--- project4/code/src/Observer/logger.java
+++ project4/code/src/Observer/logger.java
@@ -1,13 +1,12 @@
 package Observer;
 
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
+//import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+//import java.nio.file.Path;
+//import java.nio.file.Paths;
 import java.util.Objects;
 
 import Store.Store;
@@ -28,9 +27,6 @@ public class logger implements Observer, Writer, Display {
     //String name1="project4//logs//North//Logger-"+North_store.get_daysPassed()+".txt";
     //String name2="project4//logs//South//Logger-"+South_store.get_daysPassed()+".txt";
 
-    /*
-    *
-    * */
     //Used this link to see example of lazy loading singleton
     //https://www.tutorialkart.com/java/singleton-class-in-java/
     private logger() {
@@ -39,12 +35,12 @@ public class logger implements Observer, Writer, Display {
     public static logger GetInstance() {
         if (instance==null){
             instance = new logger();
-            System.out.print("logger instantiated");
+            //System.out.print("logger instantiated");
         }
         return instance;
     }
 
-
+    //creates a file depending on the store parameter
     public void makeTxt(Store Store) {
         if (Objects.equals(Store.get_location(), "North")){
             str1 = "project4//logs//North//Logger-"+Store.get_daysPassed()+".txt";
@@ -79,9 +75,6 @@ public class logger implements Observer, Writer, Display {
         this.content = content1;
         display();
         Write(Store);
-
-
-
     }
 
     //source: https://stackoverflow.com/questions/9961292/write-to-text-file-without-overwriting-in-java
@@ -109,8 +102,6 @@ public class logger implements Observer, Writer, Display {
             System.err.println(e);
         }
 
-
-
     /*
         try {
             FileWriter scribe = new FileWriter(file, true);
@@ -121,8 +112,7 @@ public class logger implements Observer, Writer, Display {
         } catch (Exception e) {
             System.err.println(e);
         }
-        */
-        /*
+
         try{
             FileWriter scribe=new FileWriter(file,true);
             BufferedWriter bufferedScribe= new BufferedWriter(scribe);
@@ -133,7 +123,6 @@ public class logger implements Observer, Writer, Display {
         }
 
          */
-
         //System.out.println(file_name);
         //System.out.println(content);
         /*
@@ -145,9 +134,7 @@ public class logger implements Observer, Writer, Display {
         }catch(Exception e){
             System.err.println(e);
         }
-
          */
-
     }
 
     public void display() {
