df18ac2357b9a44aba3e1eb79014fd94f9e8c5a8
GyuminYoum <GyuminYoum@users.noreply.github.com>
2022-03-31 00:12:32 -0600

    catan init

---
+++ catan/Player.py
@@ -0,0 +1,8 @@
+class Player:
+    def __init__(self,name):
+        self.name=name
+        self.roads=[]
+        self.settlements
+        self.town
+        self.resources=[]
+}
---
+++ catan/sim.py
@@ -0,0 +1,6 @@
+#each turn
+#each player rolls
+#town/base check
+#adjust player resources
+#give player option to build proxmity to his town
+
