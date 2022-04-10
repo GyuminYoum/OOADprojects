d3de4300c8bbf4cac1a828ff5b0175156c94759d
GyuminYoum <GyuminYoum@users.noreply.github.com>
2022-04-09 21:06:24 -0600

    initial skeleton

---
+++ catan/Settlement.py
@@ -0,0 +1,4 @@
+class Settlement:
+    def __init__(self,player):
+        self.adj=[]
+        self.color=player.color
---
+++ catan/sim.py
@@ -0,0 +1,24 @@
+#each turn
+#each player rolls
+#town/base check
+#adjust player resources
+#give player option to build proxmity to his town
+
+from Field import *
+
+board=Field((500,300),50)
+board.build()
+
+pygame.init()
+surface=pygame.display.set_mode((1000,1000))
+
+for x in board.hexlist:
+    pygame.draw.polygon(surface,x.Resource.color,x.get_coords())
+    #print(x.name, x.Resource.type)
+
+points=0
+while(points<10):
+    pygame.event.get()
+    pygame.display.flip()
+    #points+=1
+
