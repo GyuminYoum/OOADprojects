f95b04c645c47405226572d946be9d1f1656f796
GyuminYoum <GyuminYoum@users.noreply.github.com>
2022-04-12 03:37:32 -0600

    bah
    bah

--- catan/hex.py
+++ catan/hex.py
@@ -1,6 +1,6 @@
 import numpy as np
-class hex:
-    def __init__(self,origin,length):
+class Hex:
+    def __init__(self,name,origin,length):
         self.p1=origin
         self.lateral=length*np.sqrt(3)/2
         self.height=length/2
@@ -9,6 +9,10 @@ class hex:
         self.p4=(origin[0],origin[1]+2*length)
         self.p5=(self.p3[0]-2*self.lateral,self.p3[1])
         self.p6=(origin[0]-self.lateral,self.p2[1])
+        self.Resource=""
+        self.name=name
+        self.value=0
+        self.Robber= False
 
     def get_coords(self):
         return (self.p1,self.p2,self.p3,self.p4,self.p5,self.p6)
