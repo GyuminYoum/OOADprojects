9a38a446658236a7261a873c61a4410b95cc4480
GyuminYoum <GyuminYoum@users.noreply.github.com>
2022-04-12 03:32:44 -0600

    Field changed

--- catan/Card.py
+++ catan/Card.py
@@ -1,3 +1,12 @@
 class Card:
     def __init__(self, title):
-        self.name=title
+        self.name = title
+"""
+    n1 = Node("A1", (self.start[0] - 2 * lateral, self.start[1]))
+    n2 = Node("A2", (n1.coord[0] + lateral, n1.coord[1] + height))
+    n3 = Node("A3", (n2.coord[0], n2.coord[1] + self.length))
+    n4 = Node("A4", (n1.coord[0], n1.coord[1] + 2 * self.length))
+    n5 = Node("A5", (n3.coord[0] - 2 * lateral, n3.coord[1]))
+    n6 = Node("A6", (n1.coord[0] - lateral, n2.coord[1]))
+"""
+
