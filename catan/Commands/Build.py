5c8130232e17b72011cdfe6d24e7469b35b59ae5
GyuminYoum <GyuminYoum@users.noreply.github.com>
2022-04-23 04:27:29 -0600

    build finished

--- catan/Commands/Build.py
+++ catan/Commands/Build.py
@@ -17,50 +17,133 @@ class Build(Command):
             for key, value in sim.current_player.resources.items():
                 print(f'{key}: {value}')
             build_action = input('Enter a number (0: None, 1: Road, 2: Settlement, 3: City): ')
-            print(type(build_action))
+            #print(type(build_action))
+            player=sim.current_player
 
             if build_action == '0':
                 print(f'{sim.current_player.name} ended their build phase. ')
                 break
 
             elif build_action == '1':
-                selections={}
-                choice=""
-                userchoice=[]
-                user_input_road=1
-                #print("BUILDING ROAD")
-                while (user_input_road != 0 or user_input_road not in userchoice):
-                    for x in sim.current_player.roads.keys():
-                        selections[x] = []
-                        for y in x.adj:
-                            if y in sim.possible_roads[x]:
-                                selections[x].append(y)
+                if player.canBuildRoad():
+                    done1 = False
+                    while not done1:
+                        print('settlement')
+                        print("curr_player " + player.name)
+                        print("curr_roads: " + str(player.get_roadNames()))
+                        print("curr_settlements: " + str(player.get_settlementNames()))
+                        print("curr_cities: " + str(player.get_cityNames()))
+                        print("Select the location for road for " + player.name + " or enter 0 to exit building road")
+                        road_list = sim.getPossibleRoads(player.generateRoadNameList())
+                        val = input("Possible selections are " + str(road_list) + "\n")
 
-                    for x in selections.keys():
-                        for y in selections[x]:
-                            choice = x.label + y.label
-                            userchoice.append(choice)
+                        if val=="0":
+                            break
+                        if val in road_list:
+                            nodes = sim.stringToRoad(val)
+                            sim.possible_roads[nodes[0]].remove(nodes[1])
+                            sim.possible_roads[nodes[1]].remove(nodes[0])
+                            if nodes[0] not in player.roads.keys():
+                                player.roads[nodes[0]] = [nodes[1]]
+                            else:
+                                player.roads[nodes[0]].append(nodes[1])
+                            if nodes[1] not in player.roads.keys():
+                                player.roads[nodes[1]] = [nodes[0]]
+                            else:
+                                player.roads[nodes[1]].append(nodes[0])
+                            player.resources["clay"]= player.resources["clay"]-1
+                            player.resources["wood"] = player.resources["wood"]-1
+                            print(player.name+" built a road at " + val)
+                            done1 = True
 
-                    while (user_input_road != 0 and user_input_road not in userchoice):
-                        user_input_road = input(
-                            'Where would you like to build road? /n possible road locations are: ' + userchoice + ". enter 0 to exit")
-                        if (user_input_road != 0 and user_input_road not in userchoice):
-                            print("Invalid selection")
+                        else:
+                            print("Invalid Selection")
+                else:
+                    print(player.name +" doesn't have enough resources to build a road")
                 # TODO: prompt user to build road somewhere
                 # create road between 2 nodes
 
                 # sim.current_player.roads.append( new road )
 
-            elif build_action == 2:
+            elif build_action == '2':
                 print('settlement')
+                print("curr_player " + player.name)
+                print("curr_roads: " + str(player.get_roadNames()))
+                print("curr_settlements: " + str(player.get_settlementNames()))
+                print("curr_cities: " + str(player.get_cityNames()))
                 # TODO: prompt user to build settlement somewhere
                 # create settlement at 1 node
                 # sim.current_player.settlement.append( new settlement )
+                # pygame.draw.circle(self.surface, self.playerlist[0].color, coord, 10)
+                if player.canBuildSettlement():
+                    done = False
+                    while not done:
+                        #resource check: 1 brick, 1 lumber, 1 wool, 1 grain
+                        print("Select the location for settlement for " + player.name+ " or enter 0 to exit building settlement")
+                        settlement_list=sim.filterPossibleSettlement(player.generatePossibleSettlements())
+                        list1=player.generateSettlementNameList()
+                        #print("possible settlements from user: "+str(list1))
+                        settlement_names=sim.convertNodeListToNameList(settlement_list)
+                        if(len(settlement_names)==0):
+                            print("No possible location available to build a settlement")
+                            break
+                        val = input("Possible locations to build a settlement are: " + str(settlement_names) + "\n")
+                        if (val=="0"):
+                            break
+                        if sim.AvailableNodeCheck(val, sim.possible_settlements):
+                            node1 = sim.getNode(val, sim.possible_settlements)
+                            player.settlement.append(node1)
+                            sim.possible_settlements.remove(node1)
+                            for x in node1.adj:
+                                if x in sim.possible_settlements:
+                                    sim.possible_settlements.remove(x)
+                            # print('playerstart pygame draw')
+                            # pygame.draw.circle(self.surface, player.color, node1.coord, 10)
+                            player.resources["clay"] = player.resources["clay"] - 1
+                            player.resources["wood"] = player.resources["wood"] - 1
+                            player.resources["sheep"] = player.resources["sheep"] - 1
+                            player.resources["wheat"] = player.resources["wheat"] - 1
+                            print("Successfully built a settlement at location " + val + " for " + player.name)
+                            done = True
+                        else:
+                            print("Invalid selection. Please enter a valid selection")
+                else:
+                    print(player.name+" doesn't have enough resources to build a settlement")
 
-            elif build_action == 3:
-                print('city')
+            elif build_action == '3':
+                #print('city')
+                print("curr_player " + player.name)
+                print("curr_roads: " + str(player.get_roadNames()))
+                print("curr_settlements: " + str(player.get_settlementNames()))
+                print("curr_cities: " + str(player.get_cityNames()))
                 # TODO: prompt user to build city somewhere
                 # allow user to build city from any settlements in sim.current_player.settlement
+                if(len(player.settlement)==0):
+                    print(player.name+ " doesn't have any settlements to upgrade to city.")
+
+                elif player.canBuildCity():
+                    done = False
+                    while not done:
+                        print("Select the location to build a city for " + player.name)
+                        settlement_names=player.getSettlementName()
+                        val = input("Possible locations to build a city are " + str(settlement_names) + " or enter 0 to exit \n")
+                        if val=="0":
+                            break
+                        if val in settlement_names:
+                            node1 = sim.getNode(val, player.settlement)
+                            player.city.append(node1)
+                            player.settlement.remove(node1)
+                            # print('playerstart pygame draw')
+                            # pygame.draw.circle(self.surface, player.color, node1.coord, 10)
+                            print(player.name + " successfully built a city at location " + val)
+                            done = True
+                            player.resources["ore"] = player.resources["ore"] - 3
+                            player.resources["wheat"] = player.resources["wheat"] - 2
+                        else:
+                            print("Invalid selection. Please enter a valid selection")
+                else:
+                    print(player.name+" doesn't have enough resources to build a city")
+                
 
             print('Do you want to build anything else? ')
             build = bool(int(input('(1: Yes, 0: No): ')))
--- catan/sim.py
+++ catan/sim.py
@@ -5,35 +5,11 @@ from Field import *
 from cardFactory import *
 from Road import *
 import pygame
+from Commands.Build import *
+from Commands.Invoker import *
+from Commands.Command import *
 
 import numpy as np
-
-"""
-board=Field((500,300),50)
-
-board.build()
-
-pygame.init()
-surface = pygame.display.set_mode((1000, 1000))
-
-for x in board.hexlist:
-    pygame.draw.polygon(surface, x.Resource.color, x.get_coords())
-    # print(x.name, x.Resource.type)
-
-
-points = 0
-while points < 10:
-    pygame.event.get()
-    pygame.display.flip()
-    # points+=1
-points=0
-while(points<10):
-    
-    pygame.event.get()
-    pygame.display.flip()
-    #points+=1
-
-"""
 
 
 class sim:
@@ -49,7 +25,7 @@ class sim:
         self.observer = None
         self.possible_roads = {}
         self.possible_settlements = []
-        self.surface = 0
+        self.done=False
 
         # self.trade = Trade()
         # self.build = Build()
@@ -63,114 +39,93 @@ class sim:
         self.possible_roads = board.generate_roads(self.field)
 
     def initializePlayers(self):
-        while self.playercount < 3 or self.playercount > 4:
+        while self.playercount!=3 and self.playercount!=4:
+        #while self.playercount < 1:
+        
             try:
-                num = int(input("Input the number of players (2-4) \n"))
+                num = int(input("Input the number of players (3-4) \n"))
                 self.playercount = num
             except ValueError:
                 print("Please provide integer value only")
         for x in range(self.playercount):
             val = input("Provide name for Player" + str(x + 1) + ": \n")
+            #randomize color, no duplicate color
             temp = np.random.randint(0, len(self.colorlist) - 1)
             color = self.colorlist[temp]
             self.colorlist.pop(temp)
 
+            #generate player object
+            #default resources to 0
             p1 = Player(val, color)
-            p1.resources['sheep'] = 0
-            p1.resources['wood'] = 0
-            p1.resources['ore'] = 0
-            p1.resources['clay'] = 0
-            p1.resources['wheat'] = 0
-            harbor_test_node = Node('B1', 1)
-            p1.settlement.append(harbor_test_node)
+            p1.resources['sheep'] = 10
+            p1.resources['wood'] = 10
+            p1.resources['ore'] = 10
+            p1.resources['clay'] = 10
+            p1.resources['wheat'] = 10
+            #harbor_test_node = Node('B1', 1)
+            #p1.settlement.append(harbor_test_node)
             self.playerlist.append(p1)
 
             # changing resources of players to test Build and Trade
-            if x == 0:
-                self.current_player = p1
-                # TODO: remove code below when done debugging
-                p1.resources['sheep'] = 3
-                p1.resources['wood'] = 2
-                p1.resources['ore'] = 4
-                p1.roads = {}
-            if x == 1:
-                p1.resources['sheep'] = 2
-                p1.resources['wood'] = 5
+            # if x == 0:
+            #     self.current_player = p1
+            #     # TODO: remove code below when done debugging
+            #     p1.resources['sheep'] = 3
+            #     p1.resources['wood'] = 2
+            #     p1.resources['ore'] = 4
+            #     p1.roads = {}
+            # if x == 1:
+            #     p1.resources['sheep'] = 2
+            #     p1.resources['wood'] = 5
             #
 
-    def playerStart1(self, counter):
-        print('playerstart1')
-        coord = (np.random.randint(100), np.random.randint(100))
-        pygame.draw.circle(self.surface, self.playerlist[0].color, coord, 10)
-        counter += 1
+    def playerStartSettlement(self, player):
+        #pygame.draw.circle(self.surface, self.playerlist[0].color, coord, 10)
+        done = False
+        while not done:
+            print("Select the location for settlement for " + player.name)
+            val = input("Possible selections are " + str(self.getSettlementNames()) + "\n")
+            if self.AvailableNodeCheck(val, self.possible_settlements):
+                node1 = self.getNode(val, self.possible_settlements)
+                player.settlement.append(node1)
+                self.possible_settlements.remove(node1)
+                for x in node1.adj:
+                    if x in self.possible_settlements:
+                        self.possible_settlements.remove(x)
+                #print('playerstart pygame draw')
+                # pygame.draw.circle(self.surface, player.color, node1.coord, 10)
+                print("Successfully built a settlement at location "+val+" for "+player.name)
+                done = True
+            else:
+                print("Invalid selection. Please enter a valid selection")
+        return player,node1
 
-    def playerStart(self):
-        for player in self.playerlist:
-            done = False
-            while not done:
-                print("Select the location for initial settlement for " + player.name)
-                val = input("Possible selections are " + str(self.getSettlementNames()) + "\n")
-                if self.AvailableNodeCheck(val, self.possible_settlements):
-                    node1 = self.getNode(val, self.possible_settlements)
-                    player.settlement.append(node1)
-                    self.possible_settlements.remove(node1)
-                    for x in node1.adj:
-                        if x in self.possible_settlements:
-                            self.possible_settlements.remove(x)
-                    print('playerstart pygame draw')
-                    # pygame.draw.circle(self.surface, player.color, node1.coord, 10)
 
-                    done = True
-                else:
-                    print("Invalid selection. Please enter a valid selection")
 
-            done1 = False
-            while not done1:
-                print("Select the location for first road for " + player.name)
-                road_list = self.getPossibleRoads(player.generateRoadNameList())
-                val = input("Possible selections are " + str(road_list) + "\n")
-                if val in road_list:
-                    nodes = self.stringToRoad(val)
-                    self.possible_roads[nodes[0]].remove(nodes[1])
-                    self.possible_roads[nodes[1]].remove(nodes[0])
-                    player.roads[nodes[0]] = nodes[1]
-                    player.roads[nodes[1]] = nodes[0]
-                    print("Road has been built at " + val + "for " + player.name)
-                    done1 = True
+    def playerStartRoad(self,player):
+        done1 = False
+        while not done1:
+            print("Select the location for road for " + player.name)
+            road_list = self.getPossibleRoads(player.generateRoadNameList())
+            val = input("Possible selections are " + str(road_list) + "\n")
+            if val in road_list:
+                nodes = self.stringToRoad(val)
+                self.possible_roads[nodes[0]].remove(nodes[1])
+                self.possible_roads[nodes[1]].remove(nodes[0])
+                if nodes[0] not in player.roads.keys():
+                    player.roads[nodes[0]] = [nodes[1]]
                 else:
-                    print("Invalid Selection")
+                    player.roads[nodes[0]].append(nodes[1])
+                if nodes[1] not in player.roads.keys():
+                    player.roads[nodes[1]] = [nodes[0]]
+                else:
+                    player.roads[nodes[1]].append(nodes[0])
+                print("Road has been built at " + val + " for " + player.name)
+                done1 = True
+            else:
+                print("Invalid Selection")
+        return player, nodes
 
-        for player in reversed(self.playerlist):
-            done = False
-            while not done:
-                print("Select the location for 2nd initial settlement for " + player.name)
-                val = input("Possible selections are " + str(self.getSettlementNames()) + "\n")
-                if self.AvailableNodeCheck(val, self.possible_settlements):
-                    node1 = self.getNode(val, self.possible_settlements)
-                    player.settlement.append(node1)
-                    self.possible_settlements.remove(node1)
-                    for x in node1.adj:
-                        if x in self.possible_settlements:
-                            self.possible_settlements.remove(x)
-                    done = True
-                else:
-                    print("Invalid selection. Please enter a valid selection")
-            done1 = False
-            while not done1:
-                # road_list = []
-                print("Select the location for first road for " + player.name)
-                road_list = self.getPossibleRoads(player.generateRoadNameList())
-                val = input("Possible selections are " + str(road_list) + "\n")
-                if val in road_list:
-                    nodes = self.stringToRoad(val)
-                    self.possible_roads[nodes[0]].remove(nodes[1])
-                    self.possible_roads[nodes[1]].remove(nodes[0])
-                    player.roads[nodes[0]] = nodes[1]
-                    player.roads[nodes[1]] = nodes[0]
-                    print("Road has been built at " + val + "for " + player.name)
-                    done1 = True
-                else:
-                    print("Invalid Selection")
 
     def AvailableNodeCheck(self, node_name, node_list):
         for x in node_list:
@@ -197,8 +152,35 @@ class sim:
             return True
         return False
 
+    def filterPossibleSettlement(self,listofnode):
+        list1=[]
+        list2=self.getPossibleSettlementNames()
+        print("possible settlements: "+ str(list2))
+        for node in listofnode:
+            if node.label in list2:
+                list1.append(node)
+        return list1
+
+    def getPossibleSettlementNames(self):
+        list1=[]
+        for node in self.possible_settlements:
+            list1.append(node.label)
+        return list1
+
+    def convertNodeListToNameList(self, nodelist):
+        list1=[]
+        for x in nodelist:
+            if x.label not in list1:
+                list1.append(x.label)
+        return list1
+
     def roadToString(self, dict1):
-        pass
+        list1=[]
+        for key in dict1.keys():
+            for adj in dict1[key]:
+                list1.append(key.label+adj.label)
+        return list1
+
 
     def stringToRoad(self, string1):
         s1 = string1[:2]
@@ -208,12 +190,12 @@ class sim:
             node2 = self.getNode(s2, self.possible_roads.keys())
             return (node1, node2)
         else:
-            print("Invalid ")
+            print("Invalid")
             return None
 
     def getPossibleRoads(self, user_all_road_list):
         list1 = []
-        print(user_all_road_list)
+        #print(user_all_road_list)
         for road_name in user_all_road_list:
             parsed = self.stringToRoad(road_name)
             if (parsed != None):
@@ -236,23 +218,21 @@ class sim:
         for x in self.playerlist:
             self.current_player = x
             val = x.roll()
-
             # loop through all players
             # loop through each player's settlement/city
             # loop through each settlement/city's adjacentlist
             # if the hex in adjacentlist contains value equal to val
             # distribute resources to the player
-
             # x.Trade()
             # x.Build()
-
             # trade = Trade()
             # self.invoker.set_command(trade)
             # self.invoker.execute_command(self) OR execute()
-
-            # build = Build()
-            # self.invoker.set_command(build)
-            # self.invoker.execute_command(self) OR execute()
+            invoker=Invoker()
+            self.set_invoker(invoker)
+            build = Build()
+            self.invoker.set_command(build)
+            self.invoker.execute_command(self)
 
     def initialize(self):
         self.initializePlayers()
