a2e016068e0db371e7c00bb0e77210cbfcbd8bf9
GyuminYoum <GyuminYoum@users.noreply.github.com>
2022-04-24 08:40:57 -0600

    fixed merge

--- catan/main.py
+++ catan/main.py
@@ -1,10 +1,6 @@
 
-<<<<<<< HEAD
+
 from Catan import Catan
-
-
-=======
->>>>>>> main
 from sim import *
 from Commands.Trade import *
 from Commands.Invoker import *
@@ -19,10 +15,6 @@ import pygame
 import sys
 from Catan import *
 
-<<<<<<< HEAD
-
-=======
->>>>>>> main
 C1 = Catan()
 C1.initialize()
 C1.main()
